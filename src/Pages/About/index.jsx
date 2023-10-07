ca1e272abe45f84a9749dbe96f7dba91b3e54625
Xudoberdi1107 <tojiboyevxudoberdi1107@gmai.com>
2023-10-08 01:35:54 +0500

    conflict

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <div>
+      About page
+      <h1>lorem</h1>
+    </div>
+  );
 };
 
 export default About;
